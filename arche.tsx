aa8a0d9daab0b0ae9e6f6a16b3ebdbd50cd303e1
Phil <phil@upvalue.io>
2020-06-06 16:03:17 -0500

    begin

---
+++ arche.tsx
@@ -0,0 +1,10 @@
+/*
+ * arche is a single-file React component library. It is intended to be copied (or symlinked)
+ * directly into projects.
+ */
+
+
+
+
+// <Section
+export const x = 5;
---
+++ watch.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# watch.sh - watch updates to arche and copy to a directory
+ARG=$1
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+ls ${DIR}/arche.tsx ${DIR}/arche.css | entr -s "cp ${DIR}/arche.tsx ${DIR}/arche.css ${ARG}"
