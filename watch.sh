ee9d3775ce76015a4225b8f7130a2e8b403d6d57
Phil <phil@upvalue.io>
2020-12-23 19:07:47 -0600

    Commit changes

--- watch.sh
+++ watch.sh
@@ -2,4 +2,5 @@
 # watch.sh - watch updates to arche and copy to a directory
 ARG=$1
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+set -x
 ls ${DIR}/arche.tsx ${DIR}/arche.css | entr -s "cp ${DIR}/arche.tsx ${DIR}/arche.css ${ARG}"
